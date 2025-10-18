9c35e297138c643f7b6bdc459a967d32907d4940
AungHtweMaung <aunghtwemaung@securelinkmm.com>
2025-10-18 13:56:45 +0630

    setup layout

---
+++ src/pages/layouts/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/Navbar'
+
+export default function Layout() {
+    return (
+        <div>
+            <Navbar/>
+
+            <div className='max-w-6xl mx-auto p-5'>
+                <Outlet />
+            </div>
+
+        </div>
+    )
+}
